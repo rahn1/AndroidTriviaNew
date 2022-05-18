3927bd1a6e45c50550b9d16d02925ba89d41d03b
rahn <ryanahn2000@gmail.com>
2022-05-18 14:25:34 -0700

    Final

--- app/src/main/java/com/example/android/navigation/MainActivity.java
+++ app/src/main/java/com/example/android/navigation/MainActivity.java
@@ -21,7 +21,6 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
